ed5aa00499e6a12b6bea4743700b9f50cc6e7251
Johnathon Wood <jaywood128@gmail.com>
2018-11-25 19:40:00 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,11 +1,20 @@
-require 'pry'
+# require 'pry'
+# class Backer
+#
+#   attr_accessor :backed_projects
+#
+#   def initialze(name)
+#     @name = name
+#     @backed_projects = []
+#     binding.pry
+#   end
+#
+# end
 class Backer
 
   attr_accessor :backed_projects
 
   def initialze(name)
-    @name = name
-    @backed_projects = []
     binding.pry
   end
 
